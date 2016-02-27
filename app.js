bb418d206dfc73333047beb55e2bfd34844075a9
BuildTools <j26walker@gmail.com>
2016-02-27 18:40:00 -0500

    Add answer checker

--- app.js
+++ app.js
@@ -6,27 +6,62 @@ var questions = [
   {
     question: "question2",
     answers: ["a5","a6","a7","a8"]
+  },
+  {
+    question: "question3",
+    answers: ["a9","a10","a11","a12"]
   }
 
 ]
 
+var currentN = 0;
+var currentQ = questions[currentN];
+
 $(document).ready(function(){
   $("#main").hide();
   $("#submit").click(function(){
-    updateQuestions(1);
+    updateQuestions(0);
     $("#intro").fadeOut("200", function(){
       $("#main").fadeIn("200");
     })
   })
 
   $(".answerbutton").click(function(){
-    $("#main").fadeOut("200");
+    checkCorrect($(this));
+    currentN++;
+    console.log(currentN);
+    $("#main").fadeOut("200",function(){
+      updateQuestions(currentN);
+      $("#main").fadeIn("200");
+    })
   })
 });
 
+function checkCorrect(obj){
+  console.log(obj.text());
+  console.log(questions[currentN].answers[0]);
+  if(obj.text() === questions[currentN].answers[0]){
+    console.log("correct");
+  } else {
+    console.log("incorrect");
+  }
+}
+
 function updateQuestions(num){
+  var temp = questions[num].answers.slice();
+  shuffleArray(temp);
   $("#questions").text(questions[num].question);
   $(".answerbutton").each(function(i){
-      $(this).text(questions[num].answers[i]);
+      $(this).text(temp[i]);
   })
+}
+
+function shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+    return array;
 }
